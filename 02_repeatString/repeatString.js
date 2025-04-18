dd8e5335e9a5a64647a66042afd9d61de0ce0bf2
amuseDave <amusedave2@gmail.com>
2025-04-18 15:23:55 +0300

    Solve 7 starting JS odin challenges

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function (str, times) {
+  if (!times) return "";
+  if (times < 0) return "ERROR";
 
+  const mainStr = str;
+
+  for (let i = 1; i < times; i++) {
+    str += mainStr;
+  }
+
+  return str;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  if (typeof num1 !== "number" || typeof num2 !== "number") return "ERROR";
+  if (num1 < 0 || num2 < 0) return "ERROR";
+  if (`${num1}`.includes(".") || `${num2}`.includes(".")) return "ERROR";
 
+  let startRange = num1 > num2 ? num2 : num1;
+  let endRange = startRange === num1 ? num2 : num1;
+
+  let sum = num1 + num2;
+
+  for (let i = startRange + 1; i < endRange; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
